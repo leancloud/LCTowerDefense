3407934a38683043f439c62093ec17ca37d03242
oneRain <onerain88@gmail.com>
2021-01-04 16:24:13 +0800

    feat: login and leaderboard.

---
+++ Assets/LeanCloud/Script/LCManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using LeanCloud;
+
+public class LCManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        LCApplication.Initialize("iApERatxkBFcbuIOjnQrb8eK-gzGzoHsz",
+            "oS2bLixYBn7E7ftuhcrOWaJJ",
+            "https://iaperatx.lc-cn-n1-shared.com");
+        LCLogger.LogDelegate = (LCLogLevel level, string message) => {
+            switch (level) {
+                case LCLogLevel.Debug:
+                    Debug.Log(message);
+                    break;
+                case LCLogLevel.Warn:
+                    Debug.LogWarning(message);
+                    break;
+                case LCLogLevel.Error:
+                    Debug.LogError(message);
+                    break;
+                default:
+                    break;
+            }
+        };
+    }
+}
